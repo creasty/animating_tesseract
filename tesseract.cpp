a8b8c1f1cf13a3de678f17afc946c5023b82b328
Yuki Iwanaga <yuki@creasty.com>
2015-01-17 14:04:18 +0900

    Refactoring. Draw faces

--- tesseract.cpp
+++ tesseract.cpp
@@ -11,6 +11,9 @@
 #define CUBE_SIZE_A 0.5f
 #define CUBE_SIZE_B 0.2f
 
+#define WIN_WIDTH  1000
+#define WIN_HEIGHT 1000
+
 #define TRANSFORMATION_SPEED 0.004
 #define ROTATION_SPEED       0.0017
 
@@ -109,9 +112,9 @@ static double ease_circular_in_out(double t)
  * Set parameters of `k1`, `k2`, `x` for specified time of `t`
  *
  * @param {double} t - time
- * @param {double} k1 - a transition coefficient
- * @param {double} k2 - a transition coefficient esp. used with `transit_x`
- * @param {double} x - a inner cube's move around x-axis
+ * @param {double *} k1 - a transition coefficient
+ * @param {double *} k2 - a transition coefficient esp. used with `transit_x`
+ * @param {double *} x - a inner cube's move around x-axis
  */
 static void set_parameters(double t, double *k1, double *k2, double *x)
 {
@@ -154,15 +157,8 @@ static void render_tesseract()
 
   */
 
-  /// Rotation
-  double rot_x = 360.0 * ease_circular_in_out(t_rotation);
-  double rot_y = 360.0 * t_rotation;
+  int i, j;
 
-  glRotatef(rot_x, 1.0, 0.0, 0.0);
-  glRotatef(rot_y, 0.0, 1.0, 0.0);
-
-
-  /// Vertices and edges
   static double a = CUBE_SIZE_A;
   static double b = CUBE_SIZE_B;
 
@@ -170,27 +166,29 @@ static void render_tesseract()
   set_parameters(t_transformation, &k1, &k2, &x);
 
   float vertices[16][3] = {
-    /* A0 -> B0 */ { transit_s(+a, +b, k1),    transit_s(+a, +b, k1), transit_s(+a, +b, k1) },
-    /* B0 -> B4 */ { transit_x(+b, -b, k2, x), +b,                    +b },
-    /* A1 -> B1 */ { transit_s(+a, +b, k1),    transit_s(+a, +b, k1), transit_s(-a, -b, k1) },
-    /* B1 -> B5 */ { transit_x(+b, -b, k2, x), +b,                    -b },
-    /* A2 -> B2 */ { transit_s(+a, +b, k1),    transit_s(-a, -b, k1), transit_s(+a, +b, k1) },
-    /* B2 -> B6 */ { transit_x(+b, -b, k2, x), -b,                    +b },
-    /* A3 -> B3 */ { transit_s(+a, +b, k1),    transit_s(-a, -b, k1), transit_s(-a, -b, k1) },
-    /* B3 -> B7 */ { transit_x(+b, -b, k2, x), -b,                    -b },
+    /* [ 0] A0 -> B0 */ { transit_s(+a, +b, k1),    transit_s(+a, +b, k1), transit_s(+a, +b, k1) },
+    /* [ 1] A1 -> B1 */ { transit_s(+a, +b, k1),    transit_s(+a, +b, k1), transit_s(-a, -b, k1) },
+    /* [ 2] A2 -> B2 */ { transit_s(+a, +b, k1),    transit_s(-a, -b, k1), transit_s(+a, +b, k1) },
+    /* [ 3] A3 -> B3 */ { transit_s(+a, +b, k1),    transit_s(-a, -b, k1), transit_s(-a, -b, k1) },
 
-    /* A4 -> A0 */ { transit_s(-a, +a, k1),    +a,                    +a },
-    /* B4 -> A4 */ { transit_x(-b, -a, k2, x), transit_s(+b, +a, k2), transit_s(+b, +a, k2) },
-    /* A5 -> A1 */ { transit_s(-a, +a, k1),    +a,                    -a },
-    /* B5 -> A5 */ { transit_x(-b, -a, k2, x), transit_s(+b, +a, k2), transit_s(-b, -a, k2) },
-    /* A6 -> A2 */ { transit_s(-a, +a, k1),    -a,                    +a },
-    /* B6 -> A6 */ { transit_x(-b, -a, k2, x), transit_s(-b, -a, k2), transit_s(+b, +a, k2) },
-    /* A7 -> A3 */ { transit_s(-a, +a, k1),    -a,                    -a },
-    /* B7 -> A7 */ { transit_x(-b, -a, k2, x), transit_s(-b, -a, k2), transit_s(-b, -a, k2) },
+    /* [ 4] A4 -> A0 */ { transit_s(-a, +a, k1),    +a,                    +a },
+    /* [ 5] A5 -> A1 */ { transit_s(-a, +a, k1),    +a,                    -a },
+    /* [ 6] A6 -> A2 */ { transit_s(-a, +a, k1),    -a,                    +a },
+    /* [ 7] A7 -> A3 */ { transit_s(-a, +a, k1),    -a,                    -a },
+
+    /* [ 8] B0 -> B4 */ { transit_x(+b, -b, k2, x), +b,                    +b },
+    /* [ 9] B1 -> B5 */ { transit_x(+b, -b, k2, x), +b,                    -b },
+    /* [10] B2 -> B6 */ { transit_x(+b, -b, k2, x), -b,                    +b },
+    /* [11] B3 -> B7 */ { transit_x(+b, -b, k2, x), -b,                    -b },
+
+    /* [12] B4 -> A4 */ { transit_x(-b, -a, k2, x), transit_s(+b, +a, k2), transit_s(+b, +a, k2) },
+    /* [13] B5 -> A5 */ { transit_x(-b, -a, k2, x), transit_s(+b, +a, k2), transit_s(-b, -a, k2) },
+    /* [14] B6 -> A6 */ { transit_x(-b, -a, k2, x), transit_s(-b, -a, k2), transit_s(+b, +a, k2) },
+    /* [15] B7 -> A7 */ { transit_x(-b, -a, k2, x), transit_s(-b, -a, k2), transit_s(-b, -a, k2) },
   };
 
   static int edges[34][2] = {
-    {  0,  1 }, {  0,  2 }, { 0,  4 }, { 0,  8 },
+    {  0,  1 }, {  0,  2 }, { 0,  4 }, { 0, 8 },
     {  1,  3 }, {  1,  5 }, { 1,  9 },
     {  2,  3 }, {  2,  6 }, { 2, 10 },
     {  3,  7 }, {  3, 11 },
@@ -204,19 +202,84 @@ static void render_tesseract()
     { 11, 15 },
     { 12, 13 }, { 12, 14 },
     { 13, 15 },
-    { 14, 15 }
+    { 14, 15 },
   };
 
-  int i, j, v;
+  static int faces[24][4] = {
+    // Cube A
+    { 0, 1, 3, 2 },
+    { 4, 5, 7, 6 },
+    { 4, 0, 2, 6 },
+    { 5, 1, 3, 7 },
+    { 5, 1, 0, 4 },
+    { 7, 3, 2, 6 },
+
+    // Cube B
+    {  8,  9, 11, 10 },
+    { 12, 13, 15, 14 },
+    { 12,  8, 10, 14 },
+    { 13,  9, 11, 15 },
+    { 13,  9,  8, 12 },
+    { 15, 11, 10, 14 },
+
+    // Flaps
+    { 0,  1,  9,  8 },
+    { 4,  5, 13, 12 },
+    { 4,  0,  8, 12 },
+    { 5,  1,  9, 13 },
+    { 2,  3, 11, 10 },
+    { 6,  7, 15, 14 },
+    { 6,  2, 10, 14 },
+    { 7,  3, 11, 15 },
+    { 0,  8, 10,  2 },
+    { 1,  9, 11,  3 },
+    { 4, 12, 14,  6 },
+    { 5, 13, 15,  7 },
+  };
+
+
+  /// Rotate
+  double rot_x = 360.0 * ease_circular_in_out(t_rotation);
+  double rot_y = 360.0 * t_rotation;
+
+  glRotatef(rot_x, 1.0, 0.0, 0.0);
+  glRotatef(rot_y, 0.0, 1.0, 0.0);
+
+
+  /// Draw edges
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
+  glColor4f(1.0, 1.0, 1.0, 0.9);
+  glLineWidth(2.5);
 
   for (i = 0; i < 34; ++i) {
     glBegin(GL_LINES);
       for (j = 0; j < 2; ++j) {
-        v = edges[i][j];
-        glVertex3fv(vertices[v]);
+        glVertex3fv(vertices[edges[i][j]]);
       }
     glEnd();
   }
+
+  glDisable(GL_BLEND);
+
+
+  /// Draw faces
+  glDisable(GL_DEPTH_TEST);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glColor4f(0.1, 0.4, 0.5, 0.2);
+
+  for (i = 0; i < 24; ++i) {
+    glBegin(GL_POLYGON);
+      for (j = 0; j < 4; ++j) {
+        glVertex3fv(vertices[faces[i][j]]);
+      }
+    glEnd();
+  }
+
+  glEnable(GL_DEPTH_TEST);
+  glDisable(GL_BLEND);
 }
 
 
@@ -249,13 +312,11 @@ static void toggle_animation()
  */
 static void display(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   if (animation) {
     step_animation(+1);
   }
-
-  glColor3f(1.0, 1.0, 1.0);
 
   glPushMatrix();
     render_tesseract();
@@ -271,23 +332,32 @@ static void display(void)
  */
 static void init(char *progname)
 {
-  int width = 1000, height = 1000;
-  float aspect = (float) width / height;
+  float aspect = (float) WIN_WIDTH / WIN_HEIGHT;
 
   glutInitWindowPosition(0, 0);
-  glutInitWindowSize(width, height);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
   glutCreateWindow(progname);
-  glClearColor(0.0, 0.0, 0.0, 1.0);
+
+  glClearColor(0.05, 0.05, 0.07, 1.0);
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
+
   gluPerspective(45.0, aspect, 0.1, 100.0);
-  gluLookAt(0.5, 1.5, 2.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+  gluLookAt(
+    0.5, 1.5, 2.5,
+    0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0
+  );
+
   glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+  glEnable(GL_DEPTH_TEST);
   glEnable(GL_LINE_SMOOTH);
 }
 
@@ -310,6 +380,7 @@ static void keyboard_service(unsigned char key, int x, int y)
     case 'p':
       step_animation(-1);
       break;
+    case 'q':
     case 27: // <Esc>
       exit(0);
   }
